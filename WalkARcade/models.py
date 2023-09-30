8eb33635dcd4a3c6be00c9a65fa57149232abc0d
Prashanna Rajbhandari <prashanna.rajbhandari1@gmail.com>
2023-09-30 06:48:07 +0545

    feat: model setup

---
+++ WalkARcade/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class UserPoints(models.Model):
+    username = models.CharField(max_length=255, unique=True)
+    points = models.IntegerField(default=0)
+    steps = models.IntegerField(default=0)
--- WalkARcade/settings.py
+++ WalkARcade/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'WalkARcade',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
