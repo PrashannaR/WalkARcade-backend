c53c4132568bc036ad87a73138137d016c1dc4a3
Prashanna Rajbhandari <prashanna.rajbhandari1@gmail.com>
2023-09-30 07:23:26 +0545

    feat: get the list of users for the leaderboard

--- WalkARcade/views.py
+++ WalkARcade/views.py
@@ -1,12 +1,24 @@
 from django.http import JsonResponse
 from .models import UserPoints
 from .serializers import UserPointsSerilizers
- 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response 
+from rest_framework import status
 
+
+@api_view(['GET', 'POST']) 
 def leaderboards(request):
-    #users = UserPoints.objects.order_by('-points')
-    users = UserPoints.objects.all()
-    serializer = UserPointsSerilizers(users, many=True)
-    return JsonResponse({"users": serializer.data}, safe=False)
+    if request.method == 'GET':
+        #users = UserPoints.objects.all()
+        users = UserPoints.objects.order_by('-points')
+        serializer = UserPointsSerilizers(users, many=True)
+        return JsonResponse({"users": serializer.data}, safe=False)
+    
+    elif request.method == 'POST':
+        serializer = UserPointsSerilizers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
 
      
