17be97a294c22bdc23b2387cb9a5cc863574938a
Prashanna Rajbhandari <prashanna.rajbhandari1@gmail.com>
2023-09-30 06:53:00 +0545

    feat: models and admin panel setup

--- WalkARcade/models.py
+++ WalkARcade/models.py
@@ -4,3 +4,6 @@ class UserPoints(models.Model):
     username = models.CharField(max_length=255, unique=True)
     points = models.IntegerField(default=0)
     steps = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.username + " " + str(self.points)
